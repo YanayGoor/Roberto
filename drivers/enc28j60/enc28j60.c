c55ae2924b319bc5c4eb6bcc8dd22d8b408cd840
Yanay Goor <yanay.goor@gmail.com>
2021-11-02 13:50:45 -0700

    WIP Add phy registers, clear_bits and bug fixes

--- drivers/enc28j60/enc28j60.c
+++ drivers/enc28j60/enc28j60.c
@@ -4,28 +4,32 @@
 
 #define REG_VALUE(reg, ...) (((union enc28j60_##reg){__VA_ARGS__}).serialized)
 
+#define WAIT_10_NS()                                                           \
+	for (int i = 0; i < 5; i++) {}
+#define WAIT_50_NS()                                                           \
+	for (int i = 0; i < 25; i++) {}
+
 static enc28j60_buff_addr_t _get_rx_size(const struct enc28j60_params *params) {
 	return ENC28J60_LAST_ADDR / (params->rx_weight + params->tx_weight) *
 		   params->rx_weight;
 }
 
-void enc28j60_init(struct enc28j60_controller enc) {
+enc28j60_buff_addr_t enc28j60_init(struct enc28j60_controller enc) {
 	enc28j60_buff_addr_t rx_size = _get_rx_size(&enc.params);
 
 	spi_slave_init(&enc.slave);
-
 	// initialize receive buffer
 	enc28j60_write_ctrl_reg(enc, ENC28J60_ERXST, ENC28J60_LAST_ADDR - rx_size);
 	enc28j60_write_ctrl_reg(enc, ENC28J60_ERXND, ENC28J60_LAST_ADDR);
+	enc28j60_write_ctrl_reg(enc, ENC28J60_ERXRDPT, ENC28J60_LAST_ADDR);
 
-	enc28j60_write_ctrl_reg(enc, ENC28J60_ERXRDPT,
-							ENC28J60_LAST_ADDR - rx_size);
+	enc28j60_write_ctrl_reg(enc, ENC28J60_ERXFCON, 0);
 
 	// initialize MAC
-	enc28j60_set_bits_ctrl_reg(enc, ENC28J60_MACON1,
-							   REG_VALUE(macon1, .marxen = 1,
-										 .rxpaus = enc.params.full_duplex,
-										 .txpaus = enc.params.full_duplex));
+	enc28j60_write_ctrl_reg(enc, ENC28J60_MACON1,
+							REG_VALUE(macon1, .marxen = 1,
+									  .rxpaus = enc.params.full_duplex,
+									  .txpaus = enc.params.full_duplex));
 	enc28j60_write_ctrl_reg(enc, ENC28J60_MACON3,
 							REG_VALUE(macon3, .padcfg = 0, .txcrcen = 0,
 									  .frmlnen = 0,
@@ -38,17 +42,26 @@ void enc28j60_init(struct enc28j60_controller enc) {
 	enc28j60_write_ctrl_reg(enc, ENC28J60_MAIPG,
 							ENC28J60_IPG_DEFAULT(enc.params.full_duplex));
 
+	enc28j60_write_phy_ctrl_reg(
+		enc, 0, REG_VALUE(phcon1, .pdpxmd = enc.params.full_duplex));
+
 	// the documentation does not specify why we need to configure the MAC
 	// address. since this is currently only a switch, I will leave the mac
 	// address at 0.
+
+	enc28j60_write_ctrl_reg(enc, ENC28J60_ECON1, REG_VALUE(econ1, .rxen = 1));
+	return ENC28J60_LAST_ADDR - rx_size;
 }
 
 void enc28j60_reset(struct enc28j60_controller enc) {
 	SPI_SELECT_SLAVE(&enc.slave, {
-		spi_wait_write_ready(enc.module);
+		WAIT_50_NS() // Tcss
 		spi_write(enc.module, ENC28J60_SRC());
-		spi_wait_write_ready(enc.module);
+		spi_wait_read_ready(enc.module);
+		spi_read(enc.module);
+		WAIT_10_NS() // Tcsh
 	})
+	WAIT_50_NS() // Tcsd
 }
 
 int enc28j60_receive_packet(struct enc28j60_controller enc,
@@ -60,7 +73,7 @@ int enc28j60_receive_packet(struct enc28j60_controller enc,
 	SPI_SELECT_SLAVE(&enc.slave, {
 		enc28j60_begin_buff_read(enc);
 
-		enc28j60_buff_read(enc, (uint8_t *)header, sizeof(header));
+		enc28j60_buff_read(enc, (uint8_t *)header, sizeof(*header));
 		if (header->byte_count > size) {
 			// TODO: figure out a better way to handle errors without losing the
 			//  nice context manager syntax.
@@ -70,10 +83,16 @@ int enc28j60_receive_packet(struct enc28j60_controller enc,
 		enc28j60_buff_read(enc, buffer, header->byte_count);
 	})
 
-	enc28j60_write_ctrl_reg(enc, ENC28J60_ERXRDPT, header->next_pkt_ptr);
+	// TODO: the -1 is an ugly hack to solve a deadlock, find a better solution.
+	enc28j60_write_ctrl_reg(enc, ENC28J60_ERXRDPT, header->next_pkt_ptr - 1);
 	enc28j60_set_bits_ctrl_reg(enc, ENC28J60_ECON2,
 							   REG_VALUE(econ2, .pktdec = 1));
-	return 0;
+	if (enc28j60_read_ctrl_reg(enc, ENC28J60_EIR) & 1) {
+		enc28j60_clear_bits_ctrl_reg(enc, ENC28J60_EIR, 1);
+		// TODO: check if this is actually what I need to do
+		enc28j60_clear_bits_ctrl_reg(enc, ENC28J60_EPKTCNT, -1);
+	}
+	return header->byte_count;
 }
 
 void enc28j60_transmit_packet(struct enc28j60_controller enc,
--- drivers/enc28j60/internal.c
+++ drivers/enc28j60/internal.c
@@ -13,24 +13,43 @@
 
 #define TO_HIGH_BYTE(hword) (hword << 8)
 
+#define WAIT_210_NS()                                                          \
+	for (int i = 0; i < 105; i++) {}
+#define WAIT_10_US()                                                           \
+	for (int i = 0; i < 1005; i++) {}
+
 static void _write_ctrl_reg(struct enc28j60_controller enc, uint8_t address,
 							uint8_t value) {
 	SPI_SELECT_SLAVE(&enc.slave, {
-		spi_wait_write_ready(enc.module);
+		WAIT_210_NS(); // Tcsh
 		spi_write(enc.module, ENC28J60_WCR(address));
 		spi_wait_write_ready(enc.module);
 		spi_write(enc.module, value);
-		spi_wait_write_ready(enc.module);
+		spi_wait_not_busy(enc.module);
+		spi_read(enc.module);
+		WAIT_210_NS(); // Tcsh
 	})
 }
 
-static uint8_t _read_ctrl_reg(struct enc28j60_controller enc, uint8_t address) {
+static uint8_t _read_ctrl_reg(struct enc28j60_controller enc, uint8_t address,
+							  bool dummy_byte) {
 	uint8_t result;
 	SPI_SELECT_SLAVE(&enc.slave, {
-		spi_wait_write_ready(enc.module);
+		WAIT_210_NS(); // Tcsh
 		spi_write(enc.module, ENC28J60_RCR(address));
+		spi_wait_write_ready(enc.module);
+		spi_write(enc.module, 0);
+		spi_wait_read_ready(enc.module);
+		spi_read(enc.module);
+		if (dummy_byte) {
+			spi_wait_write_ready(enc.module);
+			spi_write(enc.module, 0);
+			spi_wait_read_ready(enc.module);
+			spi_read(enc.module);
+		}
 		spi_wait_read_ready(enc.module);
 		result = spi_read(enc.module);
+		WAIT_210_NS(); // Tcsh
 	})
 	return result;
 }
@@ -38,25 +57,42 @@ static uint8_t _read_ctrl_reg(struct enc28j60_controller enc, uint8_t address) {
 static void _set_bits_ctrl_reg(struct enc28j60_controller enc, uint8_t address,
 							   uint8_t value) {
 	SPI_SELECT_SLAVE(&enc.slave, {
-		spi_wait_write_ready(enc.module);
 		spi_write(enc.module, ENC28J60_BFS(address));
 		spi_wait_write_ready(enc.module);
 		spi_write(enc.module, value);
+		spi_wait_not_busy(enc.module);
+		spi_read(enc.module);
+		WAIT_210_NS(); // Tcsh
+	})
+}
+
+static void _clear_bits_ctrl_reg(struct enc28j60_controller enc,
+								 uint8_t address, uint8_t value) {
+	SPI_SELECT_SLAVE(&enc.slave, {
+		spi_write(enc.module, ENC28J60_BFC(address));
 		spi_wait_write_ready(enc.module);
+		spi_write(enc.module, value);
+		spi_wait_not_busy(enc.module);
+		spi_read(enc.module);
+		WAIT_210_NS(); // Tcsh
 	})
 }
 
 static void _select_bank(struct enc28j60_controller enc, uint8_t bank) {
-	uint8_t econ1 = _read_ctrl_reg(enc, ENC28J60_ECON1.address);
-	_write_ctrl_reg(enc, ENC28J60_ECON1.address, econ1 | (bank & 0x3));
+	_set_bits_ctrl_reg(enc, ENC28J60_ECON1.address, bank & 0x3);
+	_clear_bits_ctrl_reg(enc, ENC28J60_ECON1.address, (~bank) & 0x3);
 }
 
 void enc28j60_begin_buff_read(struct enc28j60_controller enc) {
 	spi_wait_write_ready(enc.module);
 	spi_write(enc.module, ENC28J60_RBM());
+	spi_wait_read_ready(enc.module);
+	spi_read(enc.module);
 }
 
 uint8_t enc28j60_buff_read_byte(struct enc28j60_controller enc) {
+	spi_wait_write_ready(enc.module);
+	spi_write(enc.module, 1);
 	spi_wait_read_ready(enc.module);
 	return spi_read(enc.module);
 }
@@ -101,12 +137,41 @@ void enc28j60_set_bits_ctrl_reg(struct enc28j60_controller enc,
 	}
 }
 
+void enc28j60_clear_bits_ctrl_reg(struct enc28j60_controller enc,
+								  struct enc28j60_ctrl_reg reg,
+								  uint16_t value) {
+	if (!reg.shared) { _select_bank(enc, reg.bank); }
+	_clear_bits_ctrl_reg(enc, reg.address, LOW_BYTE(value));
+	if (reg.wide) {
+		_clear_bits_ctrl_reg(enc, reg.address + 1, HIGH_BYTE(value));
+	}
+}
+
 uint16_t enc28j60_read_ctrl_reg(struct enc28j60_controller enc,
 								struct enc28j60_ctrl_reg reg) {
 	if (!reg.shared) { _select_bank(enc, reg.bank); }
-	uint16_t value = _read_ctrl_reg(enc, reg.address);
+	uint16_t value = _read_ctrl_reg(enc, reg.address, reg.dummy_byte);
 	if (reg.wide) {
-		value |= TO_HIGH_BYTE(_read_ctrl_reg(enc, reg.address + 1));
+		value |=
+			TO_HIGH_BYTE(_read_ctrl_reg(enc, reg.address + 1, reg.dummy_byte));
 	}
 	return value;
+}
+
+uint16_t enc28j60_read_phy_ctrl_reg(struct enc28j60_controller enc,
+									uint8_t addr) {
+	enc28j60_write_ctrl_reg(enc, ENC28J60_MIREGADR, addr);
+	enc28j60_write_ctrl_reg(enc, ENC28J60_MICMD, 1);
+	WAIT_10_US()
+	while (enc28j60_read_ctrl_reg(enc, ENC28J60_MISTAT) & 1) {}
+	enc28j60_write_ctrl_reg(enc, ENC28J60_MICMD, 0);
+	return enc28j60_read_ctrl_reg(enc, ENC28J60_MIRD);
+}
+
+void enc28j60_write_phy_ctrl_reg(struct enc28j60_controller enc, uint8_t addr,
+								 uint16_t value) {
+	enc28j60_write_ctrl_reg(enc, ENC28J60_MIREGADR, addr);
+	enc28j60_write_ctrl_reg(enc, ENC28J60_MIWR, value);
+	WAIT_10_US()
+	while (enc28j60_read_ctrl_reg(enc, ENC28J60_MISTAT) & 1) {}
 }
